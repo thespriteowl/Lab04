0924748b0a2b8e2eea21e72664fc645bfb4d347e
thespriteowl <60306385+thespriteowl@users.noreply.github.com>
2020-02-24 08:19:20 -0800

    Initial commit, digital clock
    digital clock (not including world clock) implemented

---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    timer(new QTimer)
+{
+    ui->setupUi(this);
+
+    connect(timer, SIGNAL(timeout()), this, SLOT(setCurrentTime()));
+
+    setCurrentTime();
+    timer->start(1000);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setCurrentTime()
+{
+    QTime time = QTime::currentTime();
+    QString hour = time.toString("hh");
+    QString minute = time.toString("mm");
+    QString second = time.toString("ss");
+
+    ui->hourLCD->display(hour);
+    ui->minuteLCD->display(minute);
+    ui->secondLCD->display(second);
+
+}
+
